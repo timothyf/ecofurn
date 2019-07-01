5a6fa3d093e3f82a446709b9d13eba803ea3d506
timothyf <timothyf@gmail.com>
2019-07-01 13:30:01 -0400

    updates

--- config.rb
+++ config.rb
@@ -51,8 +51,8 @@ page '/*.txt', layout: false
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  deploy.remote   = 'git@bitbucket.org:tfisher/tfisher.bitbucket.io.git' # remote name or git url, default: origin
-  deploy.branch   = 'master' # default: gh-pages
+  # deploy.remote   = 'git@bitbucket.org:tfisher/tfisher.bitbucket.io.git' # remote name or git url, default: origin
+  # deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
